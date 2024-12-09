5fb9b846d385aa936f2ad8961e6bd85a46d226ed
ledyjn <ledyjentrimeiza@gmail.com>
2024-12-09 21:31:18 +0700

    update typho

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Services from './components/Services';
 import Projects from './components/Projects';
-import Contact from './components/contact';
+import Contact from './components/Contact';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
 
